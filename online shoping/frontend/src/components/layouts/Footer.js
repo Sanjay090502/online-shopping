b0339f3a10252cb1d325830e8c4f67cfe219b643
gokulrajk122@gmail.com <122141670+Sanjay090502@users.noreply.github.com>
2024-02-25 18:51:08 +0530

    first commit

---
+++ online shoping/frontend/src/components/layouts/Footer.js
@@ -0,0 +1,10 @@
+export default function Footer(){
+    return(
+        
+    <footer className="py-1">
+    <p class="text-center text-dark mt-1">
+      Pickup Online Shoping - 2023-2024, All Rights Reserved
+    </p>
+  </footer>
+    )
+}
